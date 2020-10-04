a1a8ef038b22545c8e6e77d6443f9b7c3af1fc91
HP <nishnpj@gmail.com>
2020-10-04 16:00:59 +0530

    Nisali's third commit

--- app/src/main/java/com/example/ecommerceapp/LogoutDialog.java
+++ app/src/main/java/com/example/ecommerceapp/LogoutDialog.java
@@ -4,6 +4,7 @@ package com.example.ecommerceapp;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatDialogFragment;
